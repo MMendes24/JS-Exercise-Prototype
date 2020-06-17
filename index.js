8982822668e9024da27d042a58e925ca4f2a7cc6
MMendes24 <mmbusiness1@protonmail.com>
2020-06-17 15:03:35 -0400

    All tasks but the questions successfully completed. Long live constructor functions and OOP.

--- index.js
+++ index.js
@@ -92,9 +92,32 @@ personOne.eat('pizza');
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+  return this.tank;
+}
+
+Car.prototype.drive = function(distance){
+  const driveOne = (this.milesPerGallon * this.tank);
+  if (driveOne < distance){
+    this.odometer = this.odometer + (this.tank * this.milesPerGallon)
+    this.tank = this.tank - this.tank
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  } else {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - distance/this.milesPerGallon;
+  return `Still going strong!`
+  }
+}
+
+const carOne = new Car('Nissan Frontier', 32)
 
 /*
   TASK 3
@@ -103,9 +126,20 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
+const babyOne = new Baby('Klaus', 14, 'ball and string');
+console.log(babyOne.play());
 
 /* 
   TASK 4
